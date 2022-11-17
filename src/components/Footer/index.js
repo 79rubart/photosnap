3b9474b8a451a88060106290fdec38a595f97607
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-11-17 17:45:13 +0900

    feat(footer): add link to social

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
-import logo from '../../assets/img/logo.png'
+import { Link } from 'react-router-dom'
+import logo from '../../assets/img/logo_white.png'
 import facebook from '../../assets/img/shared/desktop/facebook.svg'
 import instagram from '../../assets/img/shared/desktop/instagram.svg'
 import pinterest from '../../assets/img/shared/desktop/pinterest.svg'
@@ -8,28 +8,36 @@ import twitter from '../../assets/img/shared/desktop/twitter.svg'
 import youtube from '../../assets/img/shared/desktop/youtube.svg'
 import BtnArrow from '../BtnArrow'
 
+import './Footer.css'
+
 const Footer = () => {
     return (
         <footer className='l-footer'>
-            <img src={logo} alt='logo' />
+            <div className='l-footer__logo-nav'>
 
-            <div className='l-footer__social'>
-                <img src={facebook} alt='facebook' />
-                <img src={youtube} alt='youtube' />
-                <img src={twitter} alt='twitter' />
-                <img src={pinterest} alt='pinterest' />
-                <img src={instagram} alt='instagram' />
+                <div className='l-footer__logo-social'>
+                    <img className='l-footer__logo' src={logo} alt='logo' />
+                    
+                    <div className='l-footer__social'>
+                        <Link to=''><img src={facebook} alt='facebook' /></Link>
+                        <Link to=''><img src={youtube} alt='youtube' /></Link>
+                        <Link to=''> <img src={twitter} alt='twitter' /></Link>
+                        <Link to=''><img src={pinterest} alt='pinterest' /></Link>
+                        <Link to=''><img src={instagram} alt='instagram' /></Link>
+                    </div>
+                </div>
+
+                <ul className='l-footer__nav'>
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/stories'>Stories</Link></li>
+                    <li><Link to='/features'>Features</Link></li>
+                    <li><Link to='/pricing'>Pricing</Link></li>
+                </ul>
+
             </div>
 
-            <ul className='l-footer__nav'>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/stories'>Stories</Link></li>
-                <li><Link to='/features'>Features</Link></li>
-                <li><Link to='/pricing'>Pricing</Link></li>
-            </ul>
-
             <div className='l-footer__call-action'>
-                <BtnArrow text='Get an invite' color='white'/>
+                <BtnArrow text='Get an invite' color='white' />
                 <p className='l-footer__copy'>Copyright 2019. All Rights Reserved</p>
             </div>
 
