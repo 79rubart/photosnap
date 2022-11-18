3e8a539e1bd70e51bffe1027e8ffcf0ac650c308
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-11-18 17:30:12 +0900

    feat(navbar): add function to close menu on smaller screens

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { BrowserRouter as Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import logo from '../../assets/img/logo.png'
 import BtnDefault from '../BtnDefault'
 import './Navbar.css'
@@ -12,19 +12,22 @@ const Navbar = () => {
         <>
             <nav className='c-navbar'>
                 <Link to='/'><img className='c-navbar__item' src={logo} alt='logo' /></Link>
-                <ul className='c-navbar__menu'>
+                <ul className={!isActive ? 'c-navbar__menu' : 'c-navbar__menu c-navbar__menu--active'}>
                     <li className='c-navbar__item'>
-                        <Link to='/stories'>Stories</Link>
+                        <Link to='/' onClick={() => {setIsActive(!isActive)}}>Home</Link>
                     </li>
                     <li className='c-navbar__item'>
-                        <Link to='/features'>Features</Link>
+                        <Link to='/stories' onClick={() => {setIsActive(!isActive)}}>Stories</Link>
                     </li>
                     <li className='c-navbar__item'>
-                        <Link to='/pricing'>Pricing</Link>
+                        <Link to='/features' onClick={() => {setIsActive(!isActive)}}>Features</Link>
+                    </li>
+                    <li className='c-navbar__item'>
+                        <Link to='/pricing' onClick={() => {setIsActive(!isActive)}}>Pricing</Link>
                     </li>
                 </ul>
                 <BtnDefault bgColor='black' text='Get an invite' />
-                <div className='c-navbar__hamburger' onClick={() => {setIsActive(!isActive)}}>
+                <div className={!isActive ? 'c-navbar__hamburger' : 'c-navbar__hamburger c-navbar__hamburger--active'} onClick={() => { setIsActive(!isActive) }}>
                     <span className='c-hamburger__bar'></span>
                     <span className='c-hamburger__bar'></span>
                 </div>
