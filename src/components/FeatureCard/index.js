a6cf77f991444469b12be87480d588e1f27690fb
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-11-19 08:32:54 +0900

    feat: add option to hide featureCard

--- src/components/FeatureCard/index.js
+++ src/components/FeatureCard/index.js
@@ -2,7 +2,7 @@ import "./FeatureCard.css"
 
 const FeatureCard = (props) => {
     return (
-        <div className="c-feature-card__card">
+        <div className={!props.hide ? "c-feature-card__card c-feature-card__card--hide" : "c-feature-card__card"}>
             <img src={props.icon} alt={props.alt} />
             <div className="c-feature-card__text">
                 <h3>{props.title}</h3>
--- src/components/FeaturesSection/index.js
+++ src/components/FeaturesSection/index.js
@@ -10,50 +10,56 @@ import image from "../../assets/img/features/desktop/drag-drop.svg"
 const FeaturesSection = (props) => {
     return (
         <section className="l-features">
-            
-                <FeatureCard
-                    icon={responsive}
-                    title="100% Responsive"
-                    description="No matter which the device you’re on, our site is fully responsive and stories look beautiful on any screen."
-                />
-            
-            
-                <FeatureCard
-                    icon={noLimit}
-                    title="No Photo Upload Limit"
-                    description="Our tool has no limits on uploads or bandwidth. Freely upload in bulk and share all of your stories in one go."
-                />
-            
-            
-                <FeatureCard
-                    icon={embed}
-                    title="Available to Embed"
-                    description="Embed Tweets, Facebook posts, Instagram media, Vimeo or YouTube videos, Google Maps, and more."
-                />
-            
-        
-                <FeatureCard
-                    icon={domain}
-                    title="Custom Domain"
-                    description="With Photosnap subscriptions you can host your stories on your own domain. You can also remove our branding!"
-                />
-            
-            
-        
-                <FeatureCard
-                    icon={exposure}
-                    title="Boost Your Exposure"
-                    description="Users that viewed your story or gallery can easily get notifed of new and featured stories with our built in mailing list."
-                />
-            
-        
-                <FeatureCard
-                    icon={image}
-                    title="Drag & Drop Image"
-                    description="Easily drag and drop your image and get beautiful shots everytime. No over the top tooling to add friction to creating stories."
-                />
-            
-            
+
+            <FeatureCard
+                hide={true}
+                icon={responsive}
+                title="100% Responsive"
+                description="No matter which the device you’re on, our site is fully responsive and stories look beautiful on any screen."
+            />
+
+
+            <FeatureCard
+                hide={true}
+                icon={noLimit}
+                title="No Photo Upload Limit"
+                description="Our tool has no limits on uploads or bandwidth. Freely upload in bulk and share all of your stories in one go."
+            />
+
+
+            <FeatureCard
+                hide={true}
+                icon={embed}
+                title="Available to Embed"
+                description="Embed Tweets, Facebook posts, Instagram media, Vimeo or YouTube videos, Google Maps, and more."
+            />
+
+
+            <FeatureCard
+                hide={props.featPage}
+                icon={domain}
+                title="Custom Domain"
+                description="With Photosnap subscriptions you can host your stories on your own domain. You can also remove our branding!"
+            />
+
+
+
+            <FeatureCard
+                hide={props.featPage}
+                icon={exposure}
+                title="Boost Your Exposure"
+                description="Users that viewed your story or gallery can easily get notifed of new and featured stories with our built in mailing list."
+            />
+
+
+            <FeatureCard
+                hide={props.featPage}
+                icon={image}
+                title="Drag & Drop Image"
+                description="Easily drag and drop your image and get beautiful shots everytime. No over the top tooling to add friction to creating stories."
+            />
+
+
         </section>
     )
 }
