a77cfd3802517ed3995860b88066bc749312ccaf
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-11-19 01:44:16 +0900

    feat(home): add responsiveness images

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,32 +1,42 @@
 import MainCard from "../../components/MainCard";
-import image1 from '../../assets/img/home/desktop/create-and-share.jpg'
-import image2 from '../../assets/img/home/desktop/beautiful-stories.jpg'
-import image3 from '../../assets/img/home/desktop/designed-for-everyone.jpg'
+import FeaturesSection from '../../components/FeaturesSection'
+import image1_desktop from '../../assets/img/home/desktop/create-and-share.jpg'
+import image1_tablet from '../../assets/img/home/tablet/create-and-share.jpg'
+import image1_mobile from '../../assets/img/home/mobile/create-and-share.jpg'
+import image2_desktop from '../../assets/img/home/desktop/beautiful-stories.jpg'
+import image2_tablet from '../../assets/img/home/tablet/beautiful-stories.jpg'
+import image2_mobile from '../../assets/img/home/mobile/beautiful-stories.jpg'
+import image3_desktop from '../../assets/img/home/desktop/designed-for-everyone.jpg'
+import image3_tablet from '../../assets/img/home/tablet/designed-for-everyone.jpg'
+import image3_mobile from '../../assets/img/home/mobile/designed-for-everyone.jpg'
 
 const Home = () => {
     return (
-        <>
+        <main>
             <MainCard background="black" textPosition=""
                 title="Create and share your photo stories."
                 text="Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others."
-                img={image1} alt='man at snowy place'
+                desktop={image1_desktop} tablet={image1_tablet} mobile={image1_mobile} alt='man at snowy place'
                 btnText="Get an invite"
             />
 
             <MainCard background="white" textPosition="right"
                 title="Beautiful stories every time"
                 text="We provide design templates to ensure your stories look terrific. Easily add photos, text, embed maps and media from other networks. Then share your story with everyone."
-                img={image2} alt='laptop on the desk'
+                desktop={image2_desktop} tablet={image2_tablet} mobile={image2_mobile} alt='laptop on the desk'
                 btnText="View the stories"
             />
 
             <MainCard background="white" textPosition=""
                 title="Designed for everyone"
                 text="Photosnap can help you create stories that resonate with your audience.  Our tool is designed for photographers of all levels, brands, businesses you name it."
-                img={image3} alt='photographer'
+                desktop={image3_desktop} tablet={image3_tablet} mobile={image3_mobile} alt='photographer'
                 btnText="View the stories"
             />
-        </>
+            
+            <FeaturesSection />
+
+        </main>
     )
 }
 
