9c95cf5eac3c0a751673eaf5672f777760421bd6
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-11-19 01:37:51 +0900

    feat(featureCard): separate text from image

--- src/components/FeatureCard/index.js
+++ src/components/FeatureCard/index.js
@@ -2,10 +2,12 @@ import "./FeatureCard.css"
 
 const FeatureCard = (props) => {
     return (
-        <div className="card">
-            <img src={props.icon} alt={props.alt}/>
-            <p>{props.title}</p>
-            <span>{props.description}</span>
+        <div className="c-feature-card__card">
+            <img src={props.icon} alt={props.alt} />
+            <div className="c-feature-card__text">
+                <h3>{props.title}</h3>
+                <p>{props.description}</p>
+            </div>
         </div>
     )
 }
