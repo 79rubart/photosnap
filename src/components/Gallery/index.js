8df68efcd5fcfb088dcfe89175346adc9de086be
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-11-21 17:28:00 +0900

    feat(gallery): add request to database

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,5 +1,19 @@
 import React from 'react'
+import { useEffect, useState } from "react"
+import { getDocs, collection } from 'firebase/firestore'
+import { db } from '../../firebase/config'
 const Gallery = () => {
+
+    const [images, setImages] = useState('')
+    const imageCollectionRef = collection(db, 'photo-gallery')
+
+    useEffect(() => {
+        const getData = async () => {
+            const data = await getDocs(imageCollectionRef)
+            console.log(data)
+        }
+        getData();
+    }, [])
 
     return (
         <div>
