88f49882808071741c58ffff4f5f5daf1dee5f4e
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-11-22 18:03:07 +0900

    feat(gallery): add function to show pictures from firebase

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { useState } from "react"
+import { storage } from '../../firebase/config'
+import { getDownloadURL, ref } from 'firebase/storage'
+import { uuidv4 } from '@firebase/util'
+import useImageCollection from '../../firebase/useImageCollection'
 
 
 const Gallery = () => {
 
+    const [url, setUrl] = useState('')
 
+    return (
+        <section>
+            {useImageCollection().map((cardImage) => {
+                const reference = ref(storage, cardImage.src[0])
+                getDownloadURL(ref(storage, reference)).then((x) => {
+                    setUrl(x)
+                })
+                
+            return (
+            <div key={uuidv4}>
+                <img src={url} alt='' />
+            </div>
+        )
+            })}
+        </section>
+    )
 }
 
 export default Gallery
