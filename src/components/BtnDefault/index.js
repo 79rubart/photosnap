d536e26dca8b929fa7c0ef8100cc314e204e701b
Rubens Oliveira <rubensoliveira.prof@gmail.com>
2022-11-23 02:01:32 -0300

    feat(pricing): add button responsiveness

--- src/components/BtnDefault/index.js
+++ src/components/BtnDefault/index.js
@@ -1,9 +1,12 @@
 import "./BtnDefault.css"
 
 const BtnDefault = (props) => {
+    let bgColor = props.bgColor;
+    let width = props.width;
     return (
         <>
-            <button className={props.bgColor === 'black' ? 'c-btn__default c-btn__default--bgBlack' : 'c-btn__default c-btn__default--bgWhite'}>
+            <button className={props.bgColor === 'black' ? props.width === 'big' ? 'c-btn__default c-btn__default--bgBlack c-btn__default--big' : 'c-btn__default c-btn__default--bgBlack' : 
+            props.width === 'big' ? 'c-btn__default c-btn__default--bgWhite c-btn__default--big' : 'c-btn__default c-btn__default--bgWhite'}>
                 {props.text}
             </button>
         </>
