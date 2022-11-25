fbd93b7009e57bc959b4b991881b9cd150ef4512
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-11-25 18:38:58 +0900

    feat(gallery): get images from database

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,29 +1,43 @@
-import React from 'react'
-import { useState } from "react"
+import React, { useEffect, useState } from 'react'
 import { storage } from '../../firebase/config'
-import { getDownloadURL, ref } from 'firebase/storage'
-import { uuidv4 } from '@firebase/util'
+import { getDownloadURL, ref} from 'firebase/storage'
+import { v4 as uuidv4 } from 'uuid'
 import useImageCollection from '../../firebase/useImageCollection'
+import GalleryCard from '../GalleryCard'
+import './Gallery.css'
 
 
 const Gallery = () => {
+    const imageCollection = useImageCollection()
+    const [cardCollection, setCardCollection] = useState([])
 
-    const [url, setUrl] = useState('')
+    useEffect(() => {
+        const getImage = async () => {
+            let temp = []
+            let cardList = await imageCollection
+            cardList.map((cardImage) => {
+                const reference = ref(storage, cardImage.src[0])
+                getDownloadURL(ref(storage, reference)).then((url) => {
+                    cardImage.src = url
+                    cardImage.key = uuidv4()
+                })
+                temp.push(cardImage)
+            })
+            setCardCollection(temp);
+        }
+        getImage()
+    }, [imageCollection])
+
 
     return (
-        <section>
-            {useImageCollection().map((cardImage) => {
-                const reference = ref(storage, cardImage.src[0])
-                getDownloadURL(ref(storage, reference)).then((x) => {
-                    setUrl(x)
-                })
-                
-            return (
-            <div key={uuidv4}>
-                <img src={url} alt='' />
-            </div>
-        )
-            })}
+        <section className='c-gallery'>
+            {cardCollection.map(cardImage => (
+                (
+                    <div className='c-gallery__card' key={uuidv4()}>
+                        <GalleryCard src={cardImage.src} alt={cardImage.alt} />
+                    </div>
+                )
+            ))}
         </section>
     )
 }
