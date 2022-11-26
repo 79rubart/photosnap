651ef43d1c1ee86064eb97f49e61d5bb30bd04c4
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-11-26 13:19:00 +0900

    fix(gallery): images was not rendering

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { storage } from '../../firebase/config'
-import { getDownloadURL, ref} from 'firebase/storage'
+import { getDownloadURL, ref } from 'firebase/storage'
 import { v4 as uuidv4 } from 'uuid'
 import useImageCollection from '../../firebase/useImageCollection'
 import GalleryCard from '../GalleryCard'
@@ -12,32 +12,36 @@ const Gallery = () => {
     const [cardCollection, setCardCollection] = useState([])
 
     useEffect(() => {
-        const getImage = async () => {
-            let temp = []
-            let cardList = await imageCollection
-            cardList.map((cardImage) => {
-                const reference = ref(storage, cardImage.src[0])
-                getDownloadURL(ref(storage, reference)).then((url) => {
+        const fetchImages = () =>{
+            let result =  imageCollection.map((cardImage) => {
+                let getUrl = async () =>{
+                    const reference = await ref(storage, cardImage.src[0])
+                    const url = await getDownloadURL(ref(storage, reference))
                     cardImage.src = url
-                    cardImage.key = uuidv4()
-                })
-                temp.push(cardImage)
-            })
-            setCardCollection(temp);
-        }
-        getImage()
+                    return cardImage
+                }    
+                return getUrl()
+        })
+            return Promise.all(result)
+            }
+
+            const loadImages = async () => {
+                const urls = await fetchImages()
+                setCardCollection(urls)
+            }
+            loadImages()
     }, [imageCollection])
 
 
     return (
         <section className='c-gallery'>
-            {cardCollection.map(cardImage => (
+            {cardCollection.length !== 0 ? cardCollection.map(cardImage => (
                 (
                     <div className='c-gallery__card' key={uuidv4()}>
-                        <GalleryCard src={cardImage.src} alt={cardImage.alt} />
+                        <GalleryCard src={cardImage.src} author={cardImage.author}  alt={cardImage.alt} date={cardImage.date} title={cardImage.title} />
                     </div>
                 )
-            ))}
+            )) : ""}
         </section>
     )
 }
