76b465cfc3ddc9e6a7c3f60affc9f26a8d77183a
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-11-27 10:06:57 +0900

    feat(stories): add header

--- src/pages/Stories/index.js
+++ src/pages/Stories/index.js
@@ -1,11 +1,31 @@
 import Gallery from "../../components/Gallery"
+import StoriesHeaderCard from "../../components/StoriesHeaderCard"
+import desktop from "../../assets/img/stories/desktop/moon-of-appalacia.jpg"
+import mobile from "../../assets/img/stories/mobile/moon-of-appalacia.jpg"
+import tablet from "../../assets/img/stories/tablet/moon-of-appalacia.jpg"
 
 const Stories = () => {
-  
+
   return (
-        <div>
-          <Gallery />
-    </div>
+    <>
+      <StoriesHeaderCard
+        author='by John Appleseed'
+        background='black'
+        btnText='Read the story'
+        date='March 2nd 2020&nbsp;'
+        text='The dissected plateau area, while not actually made up of geological mountains, is popularly called "mountains," especially in eastern Kentucky and West Virginia, and while the ridges are not high, the terrain is extremely rugged.'
+        title='Hazy full moon of appalachia'
+        topText={`Last month's featured story`}
+
+        alt='Hazy full moon of appalachia'
+        desktop={desktop}
+        mobile={mobile}
+        tablet={tablet}
+      />
+      <main>
+        <Gallery />
+      </main>
+    </>
   )
 }
 
