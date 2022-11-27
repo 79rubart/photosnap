9c53e96226d8d1322617d54c4a184595271dce3e
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-11-27 19:42:48 +0900

    style: remove unused variables

--- src/components/BtnDefault/index.js
+++ src/components/BtnDefault/index.js
@@ -1,8 +1,6 @@
 import "./BtnDefault.css"
 
 const BtnDefault = (props) => {
-    let bgColor = props.bgColor;
-    let width = props.width;
     return (
         <>
             <button className={props.bgColor === 'black' ? props.width === 'big' ? 'c-btn__default c-btn__default--bgBlack c-btn__default--big' : 'c-btn__default c-btn__default--bgBlack' : 
