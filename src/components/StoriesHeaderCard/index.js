5e5eea6737d2e7f6fc63a56479c71415fe8fa6c5
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-11-27 10:06:06 +0900

    feat: add stories header

---
+++ src/components/StoriesHeaderCard/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import BtnArrow from '../BtnArrow'
+import { useMediaQuery } from 'react-responsive'
+import './StoriesHeaderCard.css'
+
+const StoriesHeaderCard = (props) => {
+
+    const desktop = useMediaQuery({ minWidth: 1441 })
+    const tablet = useMediaQuery({ minWidth: 769, maxWidth: 1440 })
+    const mobile = useMediaQuery({ maxWidth: 768 })
+
+    return (
+        <header className={props.background === 'black' ? 'c-stories__header__card c-stories__header--bgBlack' : ''}>
+            <div className='c-stories__header__text'>
+                <span className='c-stories__header__topText'>{props.topText}</span>
+                <h1>{props.title}</h1>
+                <div className='c-stories__header__dateAuthor'><p>{props.date}</p><span>{props.author}</span></div>
+                <p>{props.text}</p>
+                {
+                    props.btnText ? (
+                        <BtnArrow text={props.btnText} color={props.background === 'black' ? 'white' : 'black'} />
+                    ) : ''
+                }
+            </div>
+            {desktop && <img src={props.desktop} alt={props.alt} />}
+            {tablet && <img src={props.tablet} alt={props.alt} />}
+            {mobile && <img src={props.mobile} alt={props.alt} />}
+
+        </header>
+    )
+}
+
+export default StoriesHeaderCard
